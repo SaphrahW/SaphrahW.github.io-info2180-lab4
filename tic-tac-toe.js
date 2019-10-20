b2a1ad1270402134652d3f35a9c7a84e4eb3363c
SaphrahW <adannawint@gmail.com>
2019-10-19 19:28:52 -0500

    Part 2 and Part 3 complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -11,6 +11,39 @@ const layout = () =>{
 
     for (let i = 0; i < squares.length; ++i) {        
         squares[i].classList.add("square");
+        squares[i].addEventListener('mouseover', ()=>{
+            squares[i].classList.add("hover");
+        })
+        squares[i].addEventListener('mouseout', ()=>{
+            squares[i].classList.remove("hover");
+        })
       }
+
+    game(squares);
+
+}
+
+const game =(squares) =>{
+    let player = 0;
+    for (let i = 0; i < squares.length; ++i) {        
+        squares[i].addEventListener('click',()=>{
+            if(squares[i].innerHTML==""){
+                if(player==0){
+                    if(!squares[i].classList.contains("O")){
+                        squares[i].classList.add("X");
+                        squares[i].innerHTML= "X";
+                    }
+                    player=1;
+                }else{
+                    if(!squares[i].classList.contains("X")){
+                            squares[i].classList.add("O");
+                            squares[i].innerHTML= "O";
+                    } 
+                    player=0;                 
+                }
+            }
+        })
+      }
+
 
 }

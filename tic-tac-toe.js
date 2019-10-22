0475e23bb3a2b37de0d3b29a24e03f142ef7794e
SaphrahW <adannawint@gmail.com>
2019-10-21 23:15:18 -0500

    Perfect Game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -22,7 +22,6 @@ const layout = () =>{
             squares[i].classList.remove("hover");
         })
       }
-    
     game(squares);
     restart(squares);
 
@@ -31,52 +30,59 @@ const layout = () =>{
 const game =(squares) =>{
     let player= 0;
     let p =0;
-    let olist = "";
-    let xlist = "";
-    for (let i = 0; i < squares.length; ++i) {        
-        squares[i].addEventListener('click',()=>{
-            if(squares[i].innerHTML==""){
-                if(player==0){
-                    if(!squares[i].classList.contains("O")){
-                        squares[i].classList.add("X");
-                        squares[i].innerHTML= "X";
-                        xlist+= squares[i].getAttribute("id");
-                        p = player;
-                    }
-                    
-                    player=1;
-                }else{
-                    if(!squares[i].classList.contains("X")){
-                            squares[i].classList.add("O");
-                            squares[i].innerHTML= "O";
-                            olist+= squares[i].getAttribute("id");
+    let olist='';
+    let xlist='';
+    gameover=false;
+    for (let i = 0; i < squares.length; ++i) {    
+            const name=()=>{
+                if(squares[i].innerHTML=="" && gameover==false){ //disallow cheating
+                    if(player==0){
+                        if(!squares[i].classList.contains("O")){
+                            squares[i].classList.add("X");
+                            squares[i].innerHTML= "X";
+                            xlist=xlist+ squares[i].getAttribute("id");
                             p = player;
-                    } 
-                    player=0;              
+                        }
+                        
+                        player=1;
+                    }else{
+                        if(!squares[i].classList.contains("X")){
+                                squares[i].classList.add("O");
+                                squares[i].innerHTML= "O";
+                                olist=olist+ squares[i].getAttribute("id"); 
+                                p = player;
+                                console.log(olist);
+                        } 
+                        player=0;              
+                    }
+    
                 }
-
-            }
-            if (xlist.length>=3){
-                console.log("running check winner");
-                checkWinner(p,olist,xlist);
-            }
-        })
+                if (xlist.length>=3){
+                    gameover= checkWinner(p,olist,xlist)
+                    if (gameover==true){
+                        console.log(gameover);
+                        olist='';
+                        xlist='';
+                    }
+                }
+            }//event listener
+            squares[i].addEventListener('click',name)
     }
 }
 
 const checkWinner =(player,xlist,olist) => {
     const win_combos = ['123','456','789','147','258','369','159','357'];
     let val = false;
-    console.log(player);
+
     if (player==1){
-        console.log("checkWinner player X")
+        //console.log("checkWinner player X")
         for (let i=0;i<8;i++){
             val= searchCombos(xlist,win_combos[i]);
             if (val) break;
         }
     }else{
         if (player==0){
-            console.log("checkWinner player O")
+            //console.log("checkWinner player O")
             for (let i=0;i<8;i++){
                 val= searchCombos(olist,win_combos[i]);
                 if (val) break;
@@ -84,22 +90,21 @@ const checkWinner =(player,xlist,olist) => {
         }
     }
     if(val) announceWinner(player);
+    return val;
 }
 
 
 function searchCombos(id_str,win_string){
-    console.log("I'm in searchCombos. id_Array, win_array");
+    //console.log("I'm in searchCombos. id_Array, win_array");
     let winVal=false;
     var id_array = id_str.split('').sort();
     var win_array = win_string.split('').sort();
-    console.log(id_array)
-    console.log(win_array);
 
     if ( id_array.some( el=> win_array.includes(el) ) ){ 
-        console.log("win and idlist has something in common");
+        //console.log("win and idlist has something in common");
         win_array = win_array.filter(c => !id_array.includes(c));
-        console.log("this is win array");
-        console.log(win_array);
+        //console.log("this is win array");
+        //console.log(win_array);
         if (win_array==0){
             return true;
         }
@@ -113,15 +118,19 @@ function announceWinner(player){
     if (player ==1) winner = 'O';
     let disp = document.getElementById("status");
     disp.classList.add("you-won");
-    disp.innerHTML = ("Congratulations! " +winner+ " is the Winner!");
+    disp.innerHTML = ("Congratulations! " +winner+ " is the Winner!"); 
 }
 
 function restart(squares){
+    let disp = document.getElementById("status");
     resetBtn= document.querySelector(".btn");
     resetBtn.addEventListener("click",()=>{
+        disp.classList.remove("you-won");
+        disp.innerHTML = ("Move your mouse over a square and click to play an X or an O."); 
         for (let i = 0; i < squares.length; ++i) {        
             squares[i].innerHTML="";
             squares[i].classList=("square"); 
         }
+        game(squares);
     })
-    console.log("rest")}
+    }
